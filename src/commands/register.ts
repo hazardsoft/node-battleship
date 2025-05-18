d79b623395bf311c51a436999d5066a0b9dd0006
Henadzi Shutko <hazardsoft@gmail.com>
2025-05-18 16:23:51 +0300

    feat: try to login an user if it exists already

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -1,10 +1,17 @@
 import {ClientRequest, ClientResponse, RegisterRequstPayload, RegisterResponsePayload} from "@src/types.js";
 import {Command, ConnectionContext} from "./types.js";
-import {registerPlayer} from "../db/players.js";
+import {hasPlayer, loginPlayer, registerPlayer} from "../db/players.js";
+import {Player} from "@src/db/types.js";
 
 export const register: Command = (context: ConnectionContext, message: ClientRequest) => {
     const {name, password} = JSON.parse(message.data) as RegisterRequstPayload;
-    registerPlayer(name, password);
+    const isPlayerExist = hasPlayer(name);
+    let player: Player | null = null;
+    if (isPlayerExist) {
+        player = loginPlayer(name, password);
+    } else {
+        player = registerPlayer(name, password);
+    }
     
     const response: ClientResponse = {
         id: message.id,
@@ -12,8 +19,8 @@ export const register: Command = (context: ConnectionContext, message: ClientReq
         data: JSON.stringify({
             name,
             index: 0,
-            error: false,
-            errorText: ''
+            error: !player,
+            errorText: player ? '' : 'incorrect credentials'
         } satisfies RegisterResponsePayload),
     }
     context.connection.socket.send(JSON.stringify(response));
--- src/db/players.ts
+++ src/db/players.ts
@@ -1,14 +1,26 @@
-interface Player {
-    username: string,
-    password:string;
-}
+import {Player} from "./types.js";
 
 const players: Map<string, Player> = new Map();
 
-const registerPlayer = (username:string, password:string) => {
+const registerPlayer = (username:string, password:string): Player => {
     players.set(username, {
         username, password
     })
+    return players.get(username)!;
 }
 
-export {registerPlayer}
+const loginPlayer = (username:string, password:string): Player | null => {
+    if (players.has(username)) {
+        const player = players.get(username)!;
+        if (player.password === password) {
+            return player;
+        }
+    }
+    return null;
+}
+
+const hasPlayer = (username:string): boolean => {
+    return players.has(username);
+}
+
+export {registerPlayer, hasPlayer, loginPlayer}
