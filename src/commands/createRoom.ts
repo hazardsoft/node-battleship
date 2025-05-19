eb4a0a852363ab7457055938fccebeaac8df06f3
Henadzi Shutko <hazardsoft@gmail.com>
2025-05-20 00:28:49 +0300

    feat: add user to the created room automatically

--- src/commands/createRoom.ts
+++ src/commands/createRoom.ts
@@ -1,23 +1,16 @@
 import {Command, CommandContext} from "./types.js";
-import {addRoom} from "../db/rooms.js";
-import {ClientResponse} from "../types.js";
+import {addPlayerToRoom, addRoom} from "../db/rooms.js";
 import {updateRooms} from "../notifications/updateRooms.js";
 
 export const createRoom: Command = (context: CommandContext) => {
-    const {connectionContext, message} = context;
-
-    addRoom();
+    const {connectionContext} = context;
     if (!connectionContext.connection.playerId) {
         throw new Error(`connection does not have associated player`);
     }
 
-    const response: ClientResponse = {
-        id: message.id,
-        type: message.type,
-        data: 'Ok'
-    }
-    console.log(`--> command '${response.type}', payload ${response.data}`);
-    connectionContext.connection.socket.send(JSON.stringify(response));
-    // send updated list of rooms to ALL players
+    const room = addRoom();
+    addPlayerToRoom(room.id, connectionContext.connection.playerId);
+
+    // send updated list of rooms to all players
     updateRooms(context);
 }
